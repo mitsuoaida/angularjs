a126b8c0cb689d0da008a43fecf257b92abca954
Mitsutoshi Aida <mitsutoshi@freemind.co.jp>
2015-05-16 18:07:59 +0900

    googleServiceを追加

--- js/GoogleController.js
+++ js/GoogleController.js
@@ -1,11 +1,18 @@
-angular.module('app',[])
-  .controller('GoogleController', ['$scope', function($scope){
+var app = angular.module('app',[]);
+app.service('googleService', function() {
+  this.searchResult = function(keyword) {
+    return keyword;
+  };
+});
+
+
+app.controller('GoogleController', ['$scope', 'googleService', function($scope, googleService){
     $scope.search = function() {
-      $scope.message = $scope.keyword;
+      $scope.message = googleService.searchResult($scope.keyword);
     };
     $scope.handleKeydown = function(e) {
 	  if (e.which === 13) {
-	    $scope.message = $scope.keyword;
+      $scope.message = googleService.searchResult($scope.keyword);
 	  }
 	};
   }]);
