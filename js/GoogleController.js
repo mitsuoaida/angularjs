e2c136d93c29fffe7fae5be6113f343f15eaf83d
Mitsutoshi Aida <mitsutoshi@freemind.co.jp>
2015-05-19 10:05:22 +0900

    Google api よりデータ取得できるようにした

--- js/GoogleController.js
+++ js/GoogleController.js
@@ -1,18 +1,28 @@
 var app = angular.module('app',[]);
-app.service('googleService', function() {
-  this.searchResult = function(keyword) {
-    return keyword;
+app.factory('googleService', ['$http', function($http) {
+  var gService = {};
+  gService.search = function(keyword) {
+    return $http.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyBEfO-NT-nz9D7vOgzBasTfaUZGl0VOzV8&cx=012541465954898638347:cceu-bwi7pc&q='+keyword+'&alt=json')
+    .success(function success(data, status, headers, config){
+      return data;
+    })
+    .error(function error(data, status, headers, config){
+      alert("connect error");
+    });
   };
-});
-
+  return gService;
+}]);
 
 app.controller('GoogleController', ['$scope', 'googleService', function($scope, googleService){
     $scope.search = function() {
-      $scope.message = googleService.searchResult($scope.keyword);
+      googleService.search($scope.keyword)
+      .then(function success(result){
+        $scope.message = result;
+      });
     };
     $scope.handleKeydown = function(e) {
 	  if (e.which === 13) {
-      $scope.message = googleService.searchResult($scope.keyword);
+      $scope.message = googleService.search($scope.keyword);
 	  }
 	};
   }]);
