c9e809413759ef36195cf187fcfab92d3b37c064
Mitsutoshi Aida <mitsutoshi@freemind.co.jp>
2015-05-19 21:10:03 +0900

    リファクタリング

--- js/GoogleController.js
+++ js/GoogleController.js
@@ -1,5 +1,8 @@
 var app = angular.module('app',[]);
-app.factory('googleService', ['$http', function($http) {
+app.factory('googleService', ['$http', GoogleService])
+.controller('GoogleController', ['$scope', 'googleService', GoogleController]);
+
+function GoogleService($http) {
   var gService = {};
   gService.search = function(keyword) {
     return $http.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyBEfO-NT-nz9D7vOgzBasTfaUZGl0VOzV8&cx=012541465954898638347:cceu-bwi7pc&q='+keyword+'&alt=json')
@@ -11,18 +14,22 @@ app.factory('googleService', ['$http', function($http) {
     });
   };
   return gService;
-}]);
+}
 
-app.controller('GoogleController', ['$scope', 'googleService', function($scope, googleService){
-    $scope.search = function() {
-      googleService.search($scope.keyword)
-      .then(function success(result){
-        $scope.message = result;
-      });
-    };
-    $scope.handleKeydown = function(e) {
-	  if (e.which === 13) {
-      $scope.message = googleService.search($scope.keyword);
-	  }
-	};
-  }]);
+function GoogleController($scope, googleService) {
+  $scope.search = function() {
+    displayGoogleSearchResult($scope, googleService);
+  };
+  $scope.handleKeydown = function(e) {
+    if (e.which === 13) {
+      displayGoogleSearchResult($scope, googleService);
+    }
+  };
+}
+
+function displayGoogleSearchResult($scope, googleService) {
+  googleService.search($scope.keyword)
+  .then(function success(result){
+    $scope.message = result;
+  });
+}
